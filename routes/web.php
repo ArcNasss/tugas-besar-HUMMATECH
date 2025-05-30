25459163742c143894c86c308407574464f32683
ArcNasss <nasrililhamsa@gmail.com>
2025-05-30 20:27:47 +0700

    last but not

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\pembelianController;
 use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\DetailPenjualanController;
+use App\Http\Controllers\PenjualanController;
+
+
 
 
 Route::get('/', function () {
@@ -21,24 +25,9 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::resource('pembelis', PembeliController::class);
-
-
 Route::resource('supliers', SuplierController::class);
-
-
-
 Route::resource('kategoris', KategoriController::class);
-
 Route::resource('barangs', BarangController::class);
-
-
-
 Route::resource('pembelians', PembelianController::class);
-
-use App\Http\Controllers\PenjualanController;
-
 Route::resource('penjualans', PenjualanController::class);
-
-use App\Http\Controllers\DetailPenjualanController;
-
 Route::resource('detail-penjualans', DetailPenjualanController::class);
